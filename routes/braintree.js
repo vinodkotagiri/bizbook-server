80c5826a514fd3db01817dd5b9a726dd3e48a283
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-11-02 13:38:22 +0530

    added mailer

--- routes/braintree.js
+++ routes/braintree.js
@@ -1,18 +1,13 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { requireSignin, isAuth } = require("../controllers/auth");
-const { userById } = require("../controllers/user");
-const { generateToken, processPayment } = require("../controllers/braintree");
+const { requireSignin, isAuth } = require('../controllers/auth')
+const { userById } = require('../controllers/user')
+const { generateToken, processPayment } = require('../controllers/braintree')
 
-router.get("/braintree/getToken/:userId", requireSignin, isAuth, generateToken);
-router.post(
-    "/braintree/payment/:userId",
-    requireSignin,
-    isAuth,
-    processPayment
-);
+router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken)
+router.post('/braintree/payment/:userId', requireSignin, isAuth, processPayment)
 
-router.param("userId", userById);
+router.param('userId', userById)
 
-module.exports = router;
+module.exports = router
